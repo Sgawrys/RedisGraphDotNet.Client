00becefaa1bf7e1ebe2f900f6714ecbff0dcae4c
Stefan Gawrys <stefangawrys@gmail.com>
2019-08-30 13:33:27 -0700

    Parse properties from the redis results into the graph nodes + relations

--- src/RedisGraphDotNet.Client/Models/RedisGraphEntityResult.cs
+++ src/RedisGraphDotNet.Client/Models/RedisGraphEntityResult.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
+using StackExchange.Redis;
 
 public abstract class RedisGraphEntityResult : RedisGraphResult
 {
     public int Id { get; private set; }
 
-    public Dictionary<string, string> Properties { get; set; }
+    public Dictionary<string, RedisValue> Properties { get; set; }
 
     public RedisGraphEntityResult(int id)
     {
